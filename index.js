92658ee8a1b858c90ba8bdf1fd820b4b1d7bf9b8
tr99720 <tr99720@seznam.cz>
2026-05-16 19:46:32 +0200

    Update index.js with voice response

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/webhook", (req, res) => {
-    const message = req.body.Body;
-
-    const reply = "Odpověď: " + message;
+app.post("/voice", (req, res) => {
 
     res.set("Content-Type", "text/xml");
+
     res.send(`
         <Response>
-            <Message>${reply}</Message>
+            <Say voice="Polly.Celine">
+                Dobrý den, vítejte. Jak vám mohu pomoci?
+            </Say>
         </Response>
     `);
-});
-
-app.get("/", (req, res) => {
-    res.send("Server běží");
 });
 
 app.listen(3000);
