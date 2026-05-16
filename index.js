1f188d43607b2dd95de66be0445e0cb02a4806b6
tr99720 <tr99720@seznam.cz>
2026-05-16 20:34:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ import express from "express";
 
 const app = express();
 
-// log každého requestu (debug)
 app.use((req, res, next) => {
   console.log("➡️", req.method, req.url);
   next();
 });
 
-// homepage
 app.get("/", (req, res) => {
   res.send("OK");
 });
 
-// Twilio endpoint
 app.all("/twiml", (req, res) => {
   res.set("Content-Type", "text/xml");
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
@@ -22,7 +19,8 @@ app.all("/twiml", (req, res) => {
 </Response>`);
 });
 
-const PORT = process.env.PORT;
+// 👇 natvrdo správný port
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log("✅ Server běží na portu " + PORT);
